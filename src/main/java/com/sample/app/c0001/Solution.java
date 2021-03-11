701dffce275c1ddd4daeb7859f56f99cf8e033e7
kangxinli <kangxinli0201@163.com>
2021-03-11 18:08:57 +0800

    feat: first commit

---
+++ src/main/java/com/sample/app/c0001/Solution.java
@@ -0,0 +1,48 @@
+package com.sample.app.c0001;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	
+	public static int[] method1(int[] nums, int target) {
+		int l = nums.length;
+		int[] ans = new int[2];
+		int i, j;
+		for (i = 0; i < l - 1; i++) {
+			for (j = i + 1; j < l; j++) {
+				if (nums[i] + nums[j] == target) {
+					ans[0] = i;
+					ans[1] = j;
+				}
+			}
+		}
+		return ans;
+	}
+	
+	public static int[] method2(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (map.containsKey(target - nums[i])) {
+				return new int[]{map.get(target - nums[i]), i};
+			} else {
+				map.put(nums[i], i);
+			}
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {2,7,11,15};
+		int target = 9;
+		
+		long startTime = System.currentTimeMillis();
+//		int[] result = method1(nums, target);
+		int[] result = method2(nums, target);
+		long endTime = System.currentTimeMillis();
+		System.out.println(Arrays.toString(result));
+		System.out.println("耗时 ： " + (endTime - startTime));
+	}
+
+}
---
+++ src/main/java/com/sample/app/c0002/Solution.java
@@ -0,0 +1,57 @@
+package com.sample.app.c0002;
+
+public class Solution {
+	
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+            
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        
+        return root.next;
+    }
+	
+	public static ListNode getNode(int[] nums) {
+		ListNode root = new ListNode(nums[0]);
+		ListNode cursor = root;
+		for (int i = 1; i <= nums.length - 1; i++) {
+			ListNode numNode = new ListNode(nums[i]);
+			cursor.next = numNode;
+            cursor = numNode;
+		}
+		return root;
+	}
+
+	public static void main(String[] args) {
+		int[] n1 = {9,9,9,9,9,9,9};
+		ListNode l1 = getNode(n1);
+		
+		
+		int[] n2 = {9,9,9,9};
+		ListNode l2 = getNode(n2);
+		
+		ListNode result = addTwoNumbers(l1, l2);
+		
+		print(result);
+	}
+	
+	public static void print(ListNode node) {
+		while(node != null) {
+			System.out.print(node.val);
+			node = node.next;
+		}
+	}
+
+}
